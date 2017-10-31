439aac01e99c76e092b72233e153b5080aa1e69b
Terje Sandstrom <terje@hermit.no>
2017-10-31 16:48:45 +0100

    Added program for check of test log  #191291

---
+++ CheckTestLog/Exc.cs
@@ -0,0 +1,25 @@
+﻿namespace CheckTestLog.Test
+{
+    public class Exc
+    {
+        public Exc(string msg)
+        {
+            var parts = msg.Split(':');
+            if (parts.Length != 3)
+            {
+                Type = "Unknown";
+                Message = msg;
+            }
+            else
+            {
+                Type = parts[1].Trim().TrimStart('\'');
+                Message = parts[2].Trim();
+            }
+        }
+        public string Type { get; set; }
+        public string StackTrace { get; set; } = "";
+        public string Message { get; set; }
+
+        public string Test { get; set; }
+    }
+}

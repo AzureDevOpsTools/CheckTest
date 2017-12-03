d92bfbe15b7d33d8fb0f8644847de444bbb240e9
Terje Sandstrom <terje@hermit.no>
2017-12-03 14:07:01 +0100

    Updated and cleaned out old stuff

--- CheckTest/Properties/AssemblyInfo.cs
+++ CheckTest/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CheckTest")]
+[assembly: AssemblyTitle("KDISim.CheckTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CheckTest")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright ©  Terje Sandstrom, Kim Vedvik 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- pack.cmd
+++ pack.cmd
@@ -1,2 +1,3 @@
 md package
+msbuild checktest.sln /p:Configuration=Release /p:Platform="Any CPU"
 nuget pack CheckTest.nuspec -o package -NoPackageAnalysis
